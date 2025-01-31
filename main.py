fb2bd943cf54fd79930e2a432dde382371ec7ff2
lyszt <kalliddel@gmail.com>
2025-01-31 08:41:49 -0300

    Creates index.html for flask import

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import request, Flask, jsonify
 
 app = Flask(__name__)
-
+with open('index.html') as f:
+    index_html = f.read()
 @app.route('/')
 def index():
-    return jsonify({"message": "Active."})
-
+    return index_html
 if __name__ == '__main__':
     app.run()
