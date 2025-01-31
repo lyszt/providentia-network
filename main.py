525d99be93629cb57d88d3d7f13f17d470ccb51d
lyszt <kalliddel@gmail.com>
2025-01-31 12:21:18 -0300

    Configuring server, discord connection functional

--- main.py
+++ main.py
@@ -3,18 +3,29 @@ import json
 
 # App
 import flask
+import discord
 from flask import request, Flask, jsonify
 # Essentials
 import os
-import dotenv
+from dotenv import load_dotenv
 import logging
 # Tools
 from rich.console import Console
 from rich.markdown import Markdown
 # IMPORTED MODULES
 from Modules.Configuration.configure import *
+
+load_dotenv(dotenv_path=".env")
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+if DISCORD_TOKEN is None:
+    print("DISCORD_TOKEN is not found. Make sure the .env file is in the right location.")
 app = Flask(__name__)
 console = Console(color_system="windows")
+
+intents = discord.Intents.default()
+intents.message_content = True
+discord_client = discord.Client(intents=intents)
 
 with open('index.html') as f:
     index_html = f.read()
@@ -33,5 +44,6 @@ if __name__ == '__main__':
 # PROVIDENTIA NETWORK
 ## **PROVIDENCE**is online. Today is {datetime.datetime.now().strftime('%A, %B %d, %Y')}"""))
     Setuṕ()
+    discord_client.run(DISCORD_TOKEN)
     app.run(debug=True)
 
